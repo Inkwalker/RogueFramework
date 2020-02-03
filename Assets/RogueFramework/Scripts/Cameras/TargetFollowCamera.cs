5b3b6c50ab01b03008ac90a4444121d3193055a1
Inkwalker <mail@inkwalker.xyz>
2020-02-03 16:17:40 +0200

    Basic camera

---
+++ Assets/RogueFramework/Scripts/Cameras/TargetFollowCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace RogueFramework
+{
+    public class TargetFollowCamera : MonoBehaviour
+    {
+        [SerializeField] Transform target = default;
+
+        private void LateUpdate()
+        {
+            if (target != null)
+            {
+                float x = target.position.x;
+                float y = target.position.y;
+                float z = transform.position.z;
+
+                transform.position = new Vector3(x, y, z);
+            }
+        }
+    }
+}
