620346d1a90b2cb180ad9e7762301e3eb34f92e3
Inkwalker <mail@inkwalker.xyz>
2020-03-30 03:27:29 +0300

    Log panel

---
+++ Assets/RogueFramework/Demo/Scripts/UI/LogEntryView.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LogEntryView : MonoBehaviour
+{
+    [SerializeField] Text messageText = default;
+
+    public string Text
+    {
+        get => messageText.text;
+        set => messageText.text = value;
+    }
+}
---
+++ Assets/RogueFramework/Demo/Scripts/UI/LogView.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LogView : MonoBehaviour
+{
+    [SerializeField] LogEntryView logEntryPrefab = default;
+
+    [SerializeField] ScrollRect scrollRect = default;
+    [SerializeField] RectTransform content = default;
+
+    [SerializeField] int entriesLimit = 20;
+
+    private static LogView instance;
+    private static LogView Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = FindObjectOfType<LogView>();
+
+            return instance;
+        }
+    }
+
+    public static void Log(string message)
+    {
+        var entry = Instance.AddEntry();
+        entry.Text = message;
+
+        Instance.content.anchoredPosition = Vector2.zero;
+        Instance.scrollRect.velocity = Vector2.zero;
+    }
+
+    private LogEntryView AddEntry()
+    {
+        LogEntryView entry;
+
+        if (content.childCount < entriesLimit)
+        {
+            entry = Instantiate(logEntryPrefab, content);
+        }
+        else
+        {
+            entry = content.GetChild(0).GetComponent<LogEntryView>();
+        }
+
+        entry.transform.SetAsLastSibling();
+
+        return entry;
+    }
+
+    private void SnapTo(RectTransform target)
+    {
+        Canvas.ForceUpdateCanvases();
+
+        content.anchoredPosition =
+            (Vector2)scrollRect.transform.InverseTransformPoint(content.position)
+            - (Vector2)scrollRect.transform.InverseTransformPoint(target.position);
+    }
+
+    private IEnumerator Start()
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            yield return new WaitForSeconds(1);
+
+            Log($"Log message #{i}");
+        }
+    }
+}
